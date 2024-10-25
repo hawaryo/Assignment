eb0e13aa025bdcbabb9e00e4ca871bcf9650fc83
Ahmed <63455817+hawaryo@users.noreply.github.com>
2024-10-25 23:41:04 +0300

    feature: fix readme and add header to thrid

---
+++ src/app/thrid/README.md
@@ -0,0 +1,15 @@
+# first-assignment Home page clone
+
+### Links
+
+- **[First Assignment - Live Demo]()**
+
+The Assignment folder directory:
+
+```mermaid
+graph LR
+A[src] --> B(app) --> C(third)
+
+```
+
+**The Original Website for Reference: **
--- src/app/thrid/page.tsx
+++ src/app/thrid/page.tsx
@@ -1,1 +1,38 @@
-export default function Page() {}
+import styles from "./style.module.scss";
+export default function Page() {
+  return (
+    <>
+      <header>
+        <nav className={styles.nav}>
+          <div className={styles.logo}>Shuffle</div>
+
+          <div className={`${styles.navlinks}`}>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#Services">Services</a>
+              </li>
+              <li>
+                <a href="#Portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="#Team">Team</a>
+              </li>
+              <li>
+                <a>Dropdown</a>
+              </li>
+              <li>
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
